ded664bacced01b87b5aeed748d51cfb7081d6f4
Ciro Lemos <ciro.lemos01@gmail.com>
2019-02-18 16:37:16 +0000

    Adding devise user model

--- lib/api_version_constraint.rb
+++ lib/api_version_constraint.rb
@@ -2,7 +2,8 @@ class ApiVersionConstraint
     def initialize(options)
         @version = options[:version]
         @default = options[:default]
-
+    end
+    
     def matches?(req)
         @default || req.headers['Accept'].include?("application/vnd.taskmanager.v#{@version}")
     end
